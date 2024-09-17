dbcc2c8c6ceffed117f66e23453683da577960bd
Sachin Kumar <kumarsachinm@gmail.com>
2024-09-17 22:35:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from "react";
 
 function App() {
+  const [formData, setFormData] = useState({ fName: '', lName: '',});
+    const [submittedData, setSubmittedData] = useState(null);
+
+    const handleChange = (e) => {
+      setFormData({
+        ...formData,
+        [e.target.name]: e.target.value
+      });
+    };
+
+  const handleSubmit = (e) =>{
+    e.preventDefault(); // Prevent default form submission
+    setSubmittedData(formData);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+      <label>
+        First Name: <input required type="text" name = "fName" value={formData.fName} onChange={handleChange} />
+      </label>
+      <br/>
+      <label>
+        Last Name: <input required type="text" name = "lName" value={formData.lName} onChange={handleChange}/>
+      </label>
+      <br/>
+      <button type = "submit">Submit</button>
+      </form> 
+      {submittedData && (
+        <div>
+          <p>Full Name: {submittedData.fName +" "+ submittedData.lName}</p>
+        </div>
+      )}
+
+      
     </div>
   );
 }
